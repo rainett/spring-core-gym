cb54c1e6f038608b04571b59ec998de8372ee807
rainett <96131229+rainett@users.noreply.github.com>
2025-04-05 18:50:20 +0300

    created TrainingTypeController, dto and service

---
+++ src/main/java/com/rainett/controller/TrainingTypeController.java
@@ -0,0 +1,23 @@
+package com.rainett.controller;
+
+import com.rainett.dto.trainingtype.TrainingTypeResponse;
+import com.rainett.service.TrainingTypeService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/training-types")
+@RequiredArgsConstructor
+public class TrainingTypeController {
+    private final TrainingTypeService trainingTypeService;
+
+    @GetMapping
+    public ResponseEntity<List<TrainingTypeResponse>> getTrainingTypes() {
+        List<TrainingTypeResponse> trainingTypesResponse = trainingTypeService.getTrainingTypes();
+        return ResponseEntity.ok(trainingTypesResponse);
+    }
+}
---
+++ src/main/java/com/rainett/dto/trainingtype/TrainingTypeResponse.java
@@ -0,0 +1,9 @@
+package com.rainett.dto.trainingtype;
+
+import lombok.Data;
+
+@Data
+public class TrainingTypeResponse {
+    private Long id;
+    private String name;
+}
