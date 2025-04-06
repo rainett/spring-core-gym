11384747cb8ed23726076875f9919fac12262abb
rainett <96131229+rainett@users.noreply.github.com>
2025-04-06 14:01:17 +0300

    implemented TrainingTypeService

--- src/main/java/com/rainett/repository/TrainingTypeRepository.java
+++ src/main/java/com/rainett/repository/TrainingTypeRepository.java
@@ -1,8 +1,12 @@
 package com.rainett.repository;
 
+import com.rainett.dto.trainingtype.TrainingTypeResponse;
 import com.rainett.model.TrainingType;
+import java.util.List;
 import java.util.Optional;
 
 public interface TrainingTypeRepository extends GenericRepository<TrainingType> {
     Optional<TrainingType> findByName(String name);
+
+    List<TrainingTypeResponse> findAllDto();
 }
