42f7e3d45418fd7bbc8c262dfc147a14465f6a69
rainett <96131229+rainett@users.noreply.github.com>
2025-04-17 17:03:25 +0300

    created JWT filter

--- src/main/java/com/rainett/config/SecurityConfig.java
+++ src/main/java/com/rainett/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.rainett.config;
 
+import com.rainett.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,20 +8,21 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter filter) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(filter, BasicAuthenticationFilter.class)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(HttpMethod.POST, "/api/trainees", "/api/trainers")
                         .permitAll()
@@ -28,15 +30,6 @@ public class SecurityConfig {
                         .requestMatchers("/api/**").authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("password"))
-                .build();
-        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
---
+++ src/main/java/com/rainett/filter/JwtFilter.java
@@ -0,0 +1,97 @@
+package com.rainett.filter;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rainett.dto.ErrorResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private static final String BEARER_PREFIX = "Bearer";
+    private static final String NO_HEADER_MSG = "Missing Authorization header";
+    private static final String NO_BEARER_MSG = "Expected Authorization header to start with '"
+                                                + BEARER_PREFIX + "'";
+    private static final String BAD_TOKEN_MSG = "Token is not properly formatted";
+
+    private final ObjectMapper objectMapper;
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret:super-secret-key}")
+    private String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        try {
+            String token = extractTokenFromHeader(header);
+            authenticate(token);
+        } catch (Exception ex) {
+            sendBackResponse(ex.getMessage(), response);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void authenticate(String token) {
+        String username = JwtUtils.validateTokenAndGetUsername(token, secretKey);
+        UserDetails user = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    private String extractTokenFromHeader(String header) {
+        validateHeader(header);
+        String[] split = header.split(" ");
+        if (split.length != 2 || !split[0].equals(BEARER_PREFIX)) {
+            throw new IllegalArgumentException(BAD_TOKEN_MSG);
+        }
+        return split[1];
+    }
+
+    private static void validateHeader(String header) {
+        if (header == null) {
+            throw new IllegalArgumentException(NO_HEADER_MSG);
+        }
+        if (!header.startsWith(BEARER_PREFIX)) {
+            throw new IllegalArgumentException(NO_BEARER_MSG);
+        }
+    }
+
+    private void sendBackResponse(String message, HttpServletResponse response)
+            throws IOException {
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now().toString(),
+                HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase(), message);
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+
+    private static class JwtUtils {
+        public static String validateTokenAndGetUsername(String token, String secret) {
+            return JWT.require(Algorithm.HMAC256(secret)).build()
+                    .verify(token)
+                    .getIssuer();
+        }
+    }
+}
